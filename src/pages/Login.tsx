f772d8eda7916edc774977320e7eb5f7f8ff4766
JoeG67 <joachimgerard66@gmail.com>
2025-04-12 09:48:00 +0800

    Added in gradient background to login page, removed borders around buttons and others

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -29,14 +29,14 @@ const Login = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-avocado-600">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-t from-blue-600 to-blue-200">
       <form onSubmit={handleLogin} className="p-6 bg-white rounded shadow-md w-80">
         <h2 className="text-xl font-semibold mb-4">Login</h2>
         {error && <p className="text-red-500">{error}</p>}
 
         <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full p-2 mb-3 border rounded" />
         <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full p-2 mb-3 border rounded" />
-        <button type="submit" className="w-full text-black p-2 rounded border-2 !bg-blue-300">Login</button>
+        <button type="submit" className="w-full text-black p-2 rounded border !bg-blue-200">Login</button>
         </form>
     </div>
   );
